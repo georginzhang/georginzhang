42165b115b154da9062fca256eac91d724ad2a45
georginzhang <georginzhang@hotmail.com>
2016-08-26 15:07:20 +0800

    add

---
+++ my-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <hello1></hello1>
+    <p>Welcome to your Vue.js app! {{data}}</p>
+  </div>
+</template>
+
+<script>
+import Hello1 from './components/Hello'
+
+export default {
+  components: {
+      Hello1
+  },
+    data () {
+      return {
+        data:12123412344
+      }
+    }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1 @click="reverse(msg)">{{ msg }}</h1>
+  </div>
+</template>
+
+<!--suppress JSAnnotator -->
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!',
+    }
+  },
+  methods:{
+    reverse: function(msg){
+      this.msg = msg.split('').reverse().join('');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
