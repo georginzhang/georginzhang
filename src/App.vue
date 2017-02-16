72928768277505afaa107498f3f808c130374bb9
zhangwei <georginzhang@hotmail.com>
2017-02-16 17:11:15 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app-content">
+    <div class="header">hader</div>
+    <div class="tab">tab</div>
+    <div class="content">content</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
